ed5245ab2c1378dfd7e3c5e4a0e2c0d640cafbb6
HP <HP@DESKTOP-4Q8V1JF>
2023-03-01 15:04:07 +0530

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Xml.Schema;
+
+public class Program
+{
+    public static void Main()
+    {
+        pattern1();
+        pattern2();
+        pattern3();
+        pattern4();
+        pattern5();
+
+        static void pattern1()
+        {
+            for (int i = 1; i <= 10;  i++)
+            {
+                Console.Write(i);
+
+            }   
+            Console.WriteLine();
+
+
+        }
+       
+       static void pattern2()
+       {
+            for (int i=0; i<=20;  i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+       }
+    }
+    static void pattern3()
+    {
+        for (int i = 30; i >= 1;  i--)
+        {
+            Console.Write(i);
+        }
+        Console.WriteLine();
+    }
+    static void pattern4()
+    {
+        for (int i = 0; i<= 40; i=i+2)
+        {
+            Console.Write(i);
+        }
+        Console.WriteLine();
+    }
+    
+    static void pattern5()
+    {
+        for (int i =50; i >= 0; i = i - 2)
+        {
+            Console.Write(i);
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+
